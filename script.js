775aeae533bc43433b039f344161edcde8eda941
AartiW-12 <aartiwhaval12@gmail.com>
2025-09-26 12:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const panel = document.querySelectorAll(".panel");
+
+panel.forEach(i => {
+	i.addEventListener("click", ()=> {
+		removeActive();
+		i.classList.add("active")
+	})
+})
+
+function removeActive() {
+	panel.forEach(i => {
+		i.classList.remove("active");
+	})
+}
